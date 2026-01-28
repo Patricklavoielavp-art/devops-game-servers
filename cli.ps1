c3960b66b9658997161ede9945b0e21d3e19c7ae
Patricklavoielavp-art <Patrick_lavoie_lavp@outlook.com>
2026-01-28 18:39:40 +0000

    correction du cli.ps1

--- cli.ps1
+++ cli.ps1
@@ -27,12 +27,12 @@ switch($Game) {
 }
 
 switch ($Action) {
-    "install" {powershell.exe -ExecutionPolicy Bypass -File "$Dir\install.ps1"}
-    "update" {powershell.exe -ExecutionPolicy Bypass -File "$Dir\update.ps1"}
-    "backup" {powershell.exe -ExecutionPolicy Bypass -File "$Dir\backup.ps1"}
-    "monitor" {powershell.exe -ExecutionPolicy Bypass -File "$Dir\monitor.ps1"}
-    "start" {powershell.exe -ExecutionPolicy Bypass -File "$Dir\start_fs25.ps1"}
-    "setup" {powershell.exe -ExecutionPolicy Bypass -File "$Dir\setup_fs25.ps1"}
+    "install" {pwsh.exe -ExecutionPolicy Bypass -File "$Dir\install.ps1"}
+    "update" {pwsh.exe -ExecutionPolicy Bypass -File "$Dir\update.ps1"}
+    "backup" {pwsh.exe -ExecutionPolicy Bypass -File "$Dir\backup.ps1"}
+    "monitor" {pwsh.exe -ExecutionPolicy Bypass -File "$Dir\monitor.ps1"}
+    "start" {pwsh.exe -ExecutionPolicy Bypass -File "$Dir\start_fs25.ps1"}
+    "setup" {pwsh.exe -ExecutionPolicy Bypass -File "$Dir\setup_fs25.ps1"}
     default {
         Write-Host "Action inconue : $Action" -ForegroundColor Red
         exit 1 
