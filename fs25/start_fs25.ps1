779c9e8fe7d57d0eda6cdcf88e597d853d532b76
Patricklavoielavp-art <Patrick_lavoie_lavp@outlook.com>
2026-02-03 17:34:52 +0000

    ajoute un restructuration des dossier

--- fs25/start_fs25.ps1
+++ fs25/start_fs25.ps1
@@ -16,11 +16,11 @@ Get-Process -Name dedicatedServer -ErrorAction SilentlyContinue | Stop-Process -
 Remove-Item "$SavedDir\dedicated_server\*.db-journal" -ErrorAction SilentlyContinue
 
 # Path to executable
-$ExePath = "C:\FS25\bin\x64\FS25DedicatedServer.exe"
+$ExePath = "C:\Program Files (x86)\Steam\steamapps\common\Farming Simulator 25\dedicatedServer.exe"
 if (-not (Test-Path $ExePath)) { Write-Error "DedicatedServer.exe introuvable"; exit 1 }
 
 # Arguments
 $Args = "-saveDir `"$SavedDir`" -log"
 
 # Start the server in a new window
-Start-Process -FilePath $ExePath -ArgumentList $Args -WorkingDirectory "C:\FS25\bin\x64" -WindowStyle Normal
+Start-Process -FilePath $ExePath -ArgumentList $Args -WorkingDirectory "C:\Program Files (x86)\Steam\steamapps\common\Farming Simulator 25\x64" -WindowStyle Normal
