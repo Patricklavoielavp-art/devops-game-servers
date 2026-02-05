55ee13b5ef539be0ba0d6c4c649ae160b0eed0e5
Patricklavoielavp-art <Patrick_lavoie_lavp@outlook.com>
2026-02-05 19:14:56 +0000

    refonte de 01-windows-update.ps1

--- Windows-server-bootstrap/scripts/01-windows-update.ps1
+++ Windows-server-bootstrap/scripts/01-windows-update.ps1
@@ -69,4 +69,31 @@ Import-Module PSWindowsUpdate
 # Run Windows Update
 # ------------------------------------------------
 Write-Host "Running Windows Update..."
-Get-WindowsUpdate -AcceptAll -Install -IgnoreReboot
+Write-Host "Scanning for Windows Updates..." -ForegroundColor Cyan
+
+$updates = Get-WindowsUpdate -MicrosoftUpdate
+
+if (-not $updates) {
+    Write-Host "No updates available."
+    return
+}
+
+$total = $updates.Count
+$index = 0
+
+foreach ($update in $updates) {
+    $index++
+
+    $percent = [int](($index / $total) * 100)
+
+    Write-Progress `
+        -Activity "Installing Windows Updates" `
+        -Status "$index / $total installed" `
+        -PercentComplete $percent
+
+    Install-WindowsUpdate -Updates $update -AcceptAll -IgnoreReboot
+}
+
+Write-Progress -Activity "Installing Windows Updates" -Completed
+Write-Host "Windows Update completed." -ForegroundColor Green
+
